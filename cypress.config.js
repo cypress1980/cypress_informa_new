af4f15fae326977258dd9d0160054de315d68a12
Kailash Chander Pathak <you@example.com>
2024-06-07 13:03:26 +0530

    install cucumber dependency

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
-
+const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
   e2e: {
+    "chromeWebSecurity" : false,
+    "testFiles" : ["**/*.{feature,features}"],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+module.exports = (on,config) =>{
+  on('file:preprocessor',cucumber())
+ }
+ 
